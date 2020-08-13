112db86c3045a796abe58f3effb0c9a2127940a2
Hasan Demirtaş <utsukushihito@outlook.com>
2020-08-13 15:46:32 +0300

    Update TaskUtilities.java

--- src/main/java/tr/com/infumia/kekoutil/TaskUtilities.java
+++ src/main/java/tr/com/infumia/kekoutil/TaskUtilities.java
@@ -26,6 +26,7 @@
 package tr.com.infumia.kekoutil;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.logging.Level;
@@ -42,103 +43,72 @@ public class TaskUtilities {
     @Nullable
     private Plugin plugin;
 
-    public BukkitTask sync(@Nullable final Runnable runnable) {
+    // SYNC Methods.
+
+    @NotNull
+    public BukkitTask sync(@NotNull final Runnable runnable) {
         return new BukkitRunnable() {
             @Override
             public void run() {
                 runnable.run();
             }
-        }.runTask(TaskUtilities.plugin);
+        }.runTask(TaskUtilities.getPlugin());
     }
 
+    @NotNull
     public BukkitTask syncLater(final long delay, @NotNull final Runnable runnable) {
         return new BukkitRunnable() {
             @Override
             public void run() {
                 runnable.run();
             }
-        }.runTaskLater(TaskUtilities.plugin, delay);
+        }.runTaskLater(TaskUtilities.getPlugin(), delay);
     }
 
-    public BukkitTask async(@NotNull final Runnable runnable) {
-        return new BukkitRunnable() {
-            @Override
-            public void run() {
-                runnable.run();
-            }
-        }.runTaskAsynchronously(TaskUtilities.plugin);
-    }
-
-    public BukkitTask asyncLater(final long delay, @NotNull final Runnable runnable) {
-        return new BukkitRunnable() {
-            @Override
-            public void run() {
-                runnable.run();
-            }
-        }.runTaskLaterAsynchronously(TaskUtilities.plugin, delay);
-    }
-
+    @NotNull
     public BukkitTask syncTimer(final long period, @NotNull final Runnable runnable) {
-        return TaskUtilities.syncTimerLater(0, period, runnable);
+        return TaskUtilities.syncTimerLater(0L, period, runnable);
     }
 
+    @NotNull
+    public BukkitTask syncTimer(final long period, @NotNull final Supplier<Boolean> supplier) {
+        return TaskUtilities.syncTimerLater(0L, period, supplier);
+    }
+
+    @NotNull
+    public BukkitTask syncTimerLater(final long delay, final long period, @NotNull final Supplier<Boolean> supplier) {
+        return new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (!supplier.get()) {
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(TaskUtilities.getPlugin(), delay, period);
+    }
+
+    @NotNull
+    public <T> BukkitTask forAll(@NotNull final List<T> objects, @NotNull final Consumer<T> runArgument) {
+        return TaskUtilities.forAll(1, objects, runArgument);
+    }
+
+    @NotNull
     public BukkitTask syncTimerLater(final long delay, final long period, @NotNull final Runnable runnable) {
         return new BukkitRunnable() {
             @Override
             public void run() {
                 runnable.run();
             }
-        }.runTaskTimer(TaskUtilities.plugin, delay, period);
+        }.runTaskTimer(TaskUtilities.getPlugin(), delay, period);
     }
 
-    public BukkitTask syncTimer(final long period, @NotNull final Supplier<Boolean> runnable) {
-        return TaskUtilities.syncTimerLater(0, period, runnable);
-    }
-
-    public BukkitTask syncTimerLater(final long delay, final long period, @NotNull final Supplier<Boolean> runnable) {
-        return new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (!runnable.get()) {
-                    this.cancel();
-                }
-            }
-        }.runTaskTimer(TaskUtilities.plugin, delay, period);
-    }
-
-    public BukkitTask asyncTimer(final long period, @NotNull final Runnable runnable) {
-        return new BukkitRunnable() {
-            @Override
-            public void run() {
-                runnable.run();
-            }
-        }.runTaskTimerAsynchronously(TaskUtilities.plugin, 0L, period);
-    }
-
-    public BukkitTask asyncTimer(final long period, @NotNull final Supplier<Boolean> runnable) {
-        return TaskUtilities.asyncTimerLater(0L, period, runnable);
-    }
-
-    public BukkitTask asyncTimerLater(final long delay, final long period, @NotNull final Supplier<Boolean> runnable) {
-        return new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (!runnable.get()) {
-                    this.cancel();
-                }
-            }
-        }.runTaskTimerAsynchronously(TaskUtilities.plugin, delay, period);
-    }
-
-    public <T> BukkitTask forAll(@NotNull final List<T> objects, @NotNull final Consumer<T> runArgument) {
-        return TaskUtilities.forAll(1, objects, runArgument);
-    }
-
+    @NotNull
     public <T> BukkitTask forAll(final int perTick, final List<T> objects, @NotNull final Consumer<T> runArgument) {
         return TaskUtilities.forAll(perTick, objects, runArgument, () -> {
         });
     }
 
+    @NotNull
     public <T> BukkitTask forAll(final int perTick, @NotNull final List<T> objects,
                                  @NotNull final Consumer<T> runArgument, @NotNull final Runnable onDone) {
         return new BukkitRunnable() {
@@ -153,8 +123,8 @@ public class TaskUtilities {
                     final T object = objects.get(this.current);
                     try {
                         runArgument.accept(object);
-                    } catch (final Exception e) {
-                        TaskUtilities.plugin.getLogger().log(
+                    } catch (final RuntimeException e) {
+                        TaskUtilities.getPlugin().getLogger().log(
                             Level.SEVERE,
                             "TaskUtilities#forAll() iteration failed for object: " +
                                 object + (object != null ? " (" + object.getClass().getName() + ")" : ""),
@@ -167,11 +137,72 @@ public class TaskUtilities {
                     onDone.run();
                 }
             }
-        }.runTaskTimer(TaskUtilities.plugin, 1L, 1L);
+        }.runTaskTimer(TaskUtilities.getPlugin(), 1L, 1L);
     }
 
-    void init(@NotNull final Plugin p) {
-        TaskUtilities.plugin = p;
+    // ASYNC
+
+    @NotNull
+    public BukkitTask async(@NotNull final Runnable runnable) {
+        return new BukkitRunnable() {
+            @Override
+            public void run() {
+                runnable.run();
+            }
+        }.runTaskAsynchronously(TaskUtilities.getPlugin());
+    }
+
+    @NotNull
+    public BukkitTask asyncLater(final long delay, @NotNull final Runnable runnable) {
+        return new BukkitRunnable() {
+            @Override
+            public void run() {
+                runnable.run();
+            }
+        }.runTaskLaterAsynchronously(TaskUtilities.getPlugin(), delay);
+    }
+
+    @NotNull
+    public BukkitTask asyncTimer(final long period, @NotNull final Runnable runnable) {
+        return TaskUtilities.asyncTimerLater(0L, period, runnable);
+    }
+
+    @NotNull
+    public BukkitTask asyncTimer(final long period, @NotNull final Supplier<Boolean> supplier) {
+        return TaskUtilities.asyncTimerLater(0L, period, supplier);
+    }
+
+    @NotNull
+    public BukkitTask asyncTimerLater(final long delay, final long period, @NotNull final Supplier<Boolean> supplier) {
+        return new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (!supplier.get()) {
+                    this.cancel();
+                }
+            }
+        }.runTaskTimerAsynchronously(TaskUtilities.getPlugin(), delay, period);
+    }
+
+    @NotNull
+    public BukkitTask asyncTimerLater(final long delay, final long period, @NotNull final Runnable runnable) {
+        return new BukkitRunnable() {
+            @Override
+            public void run() {
+                runnable.run();
+            }
+        }.runTaskTimerAsynchronously(TaskUtilities.getPlugin(), delay, period);
+    }
+
+    void init(@NotNull final Plugin plugin) {
+        TaskUtilities.plugin = plugin;
+    }
+
+    @NotNull
+    private Plugin getPlugin() {
+        return Optional.ofNullable(TaskUtilities.plugin)
+            .orElseThrow(() ->
+                new RuntimeException("Use TaskUtilities#init(Plugin) first!"));
     }
 
 }
