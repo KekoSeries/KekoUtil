e5b389052d85596538a4a8219bc4eab94cc8242c
Hasan Demirtaş <utsukushihito@outlook.com>
2020-09-18 17:15:41 +0300

    fix wg utility

--- src/main/java/tr/com/infumia/kekoutil/util/NewWGUtil.java
+++ src/main/java/tr/com/infumia/kekoutil/util/NewWGUtil.java
@@ -7,50 +7,44 @@ import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.flags.Flags;
+import java.util.Optional;
+import lombok.experimental.UtilityClass;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public final class NewWGUtil {
+@UtilityClass
+public class NewWGUtil {
 
-    private NewWGUtil() {
+    public boolean canBuild(@NotNull final Player player, @NotNull final Location location) {
+        return NewWGUtil.hasBypass(player, location) ||
+            WorldGuard
+                .getInstance()
+                .getPlatform()
+                .getRegionContainer()
+                .createQuery()
+                .testState(
+                    BukkitAdapter.adapt(location),
+                    NewWGUtil.getLocalPlayer(player),
+                    Flags.BUILD);
     }
 
-    public static boolean canBuild(@NotNull final Player player, @NotNull final Location location) {
-        if (NewWGUtil.hasBypass(player, location)) {
-            return true;
-        }
-
-        return WorldGuard
-            .getInstance()
-            .getPlatform()
-            .getRegionContainer()
-            .createQuery()
-            .testState(
-                BukkitAdapter.adapt(location),
-                NewWGUtil.getLocalPlayer(player),
-                Flags.BUILD
-            );
-    }
-
-    public static boolean hasBypass(@NotNull final Player player, @NotNull final Location location) {
-        if (location.getWorld() == null) {
-            return true;
-        }
-
-        return WorldGuard.getInstance().getPlatform().getSessionManager().hasBypass(
-            NewWGUtil.getLocalPlayer(player),
-            NewWGUtil.getLocalWorld(location.getWorld())
-        );
+    public boolean hasBypass(@NotNull final Player player, @NotNull final Location location) {
+        return Optional.ofNullable(location.getWorld())
+            .map(world ->
+                WorldGuard.getInstance().getPlatform().getSessionManager().hasBypass(
+                    NewWGUtil.getLocalPlayer(player),
+                    NewWGUtil.getLocalWorld(world)))
+            .orElse(true);
     }
 
     @NotNull
-    private static LocalPlayer getLocalPlayer(@NotNull final Player player) {
+    private LocalPlayer getLocalPlayer(@NotNull final Player player) {
         return WorldGuardPlugin.inst().wrapPlayer(player);
     }
 
     @NotNull
-    private static World getLocalWorld(@NotNull final org.bukkit.World world) {
+    private World getLocalWorld(@NotNull final org.bukkit.World world) {
         return new BukkitWorld(world);
     }
 
--- src/main/java/tr/com/infumia/kekoutil/util/OldWGUtil.java
+++ src/main/java/tr/com/infumia/kekoutil/util/OldWGUtil.java
@@ -1,22 +1,24 @@
 package tr.com.infumia.kekoutil.util;
 
+import lombok.experimental.UtilityClass;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public final class OldWGUtil {
+@UtilityClass
+public class OldWGUtil {
 
-    private OldWGUtil() {
-    }
-
-    public static boolean canBuild(@NotNull final Player player, @NotNull final Location location) throws Exception {
-        final Class<?> worldGuardPluginClass = Class.forName("com.sk89q.worldguard.bukkit.WorldGuardPlugin");
-        final Object worldGuardPlugin = worldGuardPluginClass.getMethod("inst").invoke(null);
-        return (boolean) worldGuardPluginClass
-            .getMethod("canBuild", Player.class, Block.class)
-            .invoke(worldGuardPlugin, player, location.getBlock());
-
+    public boolean canBuild(@NotNull final Player player, @NotNull final Location location) {
+        try {
+            final Class<?> worldGuardPluginClass = Class.forName("com.sk89q.worldguard.bukkit.WorldGuardPlugin");
+            final Object worldGuardPlugin = worldGuardPluginClass.getMethod("inst").invoke(null);
+            return (boolean) worldGuardPluginClass
+                .getMethod("canBuild", Player.class, Block.class)
+                .invoke(worldGuardPlugin, player, location.getBlock());
+        } catch (final Throwable ignored) {
+        }
+        return true;
     }
 
 }
